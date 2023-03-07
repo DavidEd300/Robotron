e3c7ff19b73cef721bcf7344e7c0714d832359ca
David Eduardo <davideduardo300@hotmail.com>
2023-03-07 12:24:09 -0300

    docs

---
+++ Js/Main.js
@@ -0,0 +1,10 @@
+document.querySelector("#robotron").addEventListener("click", function(){
+    console.log('Cliquei no robô')
+})
+
+function dizOi(nome) {
+    console.log(nome)
+    console.log("Bem vindo ao robotron 2000.");
+}
+
+dizOi("David")
